3fdc44d6d5483853d591d11d2c8f61de4331d5aa
J Ridgeway <sw333zy@gmail.com>
2016-11-02 16:46:14 -0400

    made messages work by reworking ajax from name w/ tips from mr mat and mr jordan

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,7 +1,19 @@
 (function() {
     'use strict';
 
-    window.chat.listenForMessages(function messageHandler(data) { });
+    var token;
+    var name;
+    var message;
+
+    window.chat.listenForMessages(function messageHandler(data) {
+      console.log(data);
+      var message = $('.messages');
+      message.append('<p>' + data.message + ' ' + data.username + '</p>');
+
+    });
+
+
+
 
     $('.login')
         .on('submit', function userName(event) {
@@ -12,16 +24,21 @@
                     method: 'POST',
                     data: JSON.stringify({
                         username: $('.username').val()
+
                     }),
                     headers: {
                         'Content-Type': 'application/json',
                     }
 
                 })
-                .done(function handleMessageSucess(data) {
+                .done(function handleSucess(data) {
+                    token = data.token;
+                    name = data.username;
+                    console.log(name);
                     console.log('working', data)
+                    console.log(token)
                 })
-                .fail(function handleMessageFail(xhr) {
+                .fail(function handleFail(xhr) {
                     console.log('sorry', xhr)
                 })
 
@@ -29,9 +46,34 @@
 
         });
 
+        $('.send-message')
+            .on('submit', function message(event) {
+              event.preventDefault();
+
+                $.ajax({
+                        url: '/chat',
+                        method: 'POST',
+                        data: JSON.stringify({
+                            message: $('.message').val()
+
+                        }),
+                        headers: {
+                            Authorization: token,
+                            'Content-Type': 'application/json'
+                        }
+
+                    })
+                    .done(function handleSucess(data) {
+                        console.log(data.message)
+                        console.log(token)
+                    })
+                    .fail(function handleFail(xhr) {
+                        console.log('sorry', xhr)
+                    })
 
 
 
+ });
 
 
 
