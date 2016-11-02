c94a6a8c709a7dd9b0192564989cd95035e4c910
J Ridgeway <sw333zy@gmail.com>
2016-11-02 15:02:40 -0400

    addded in username and inputs

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,11 +1,33 @@
 (function() {
-  'use strict';
-  window.chat = window.chat || {};
+    'use strict';
 
-  window.chat.listenForMessages(function messageHandler(data) {
-console.log('hi');
+    window.chat.listenForMessages(function messageHandler(data) { });
 
-  });
+    $('.login')
+        .on('submit', function userName(event) {
+          event.preventDefault();
+
+            $.ajax({
+                    url: '/login',
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: $('.username').val()
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+
+                })
+                .done(function handleMessageSucess(data) {
+                    console.log('working', data)
+                })
+                .fail(function handleMessageFail(xhr) {
+                    console.log('sorry', xhr)
+                })
+
+
+
+        });
 
 
 
