fb35af27a76f4059d73d341bdf775411d4ec7b23
J Ridgeway <sw333zy@gmail.com>
2016-11-02 14:25:23 -0400

    files and connections made

---
+++ client/js/chat-app.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+  window.chat = window.chat || {};
+
+  window.chat.listenForMessages(function messageHandler(data) {
+console.log('hi');
+
+  });
+
+
+
+
+
+
+
+
+
+}());
